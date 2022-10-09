711dfd78c412ea95289757b3a3bab53f35f54c7a
naufaladrna08 <naufaladrn90@gmail.com>
2022-10-09 21:50:48 +0700

    added tabs

--- frontend/src/views/ArchiveView.vue
+++ frontend/src/views/ArchiveView.vue
@@ -1,16 +1,44 @@
 <template>
   <div class="container">
-    <DataTables
-      url="archive/read-all"
-      tclass="table table-bordered w-100 p-2" 
-      :columns="inboxColumn"
-      useNumber="1"
-      useAssigner="0"
-      useOpenButton="1"
-      @openClicked="openMail"
-      @actionUnarchive="unarchiveMail"
-      ref="inbox"
-    />
+    <ul class="nav nav-tabs mb-4" id="myTab" role="tablist">
+      <li class="nav-item" role="presentation">
+        <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button" role="tab" aria-controls="home" aria-selected="true"> Mail Archive </button>
+      </li>
+      <li class="nav-item" role="presentation">
+        <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="false"> File Archive </button>
+      </li>
+    </ul>
+
+    <div class="tab-content" id="myTabContent">
+      <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+        <DataTables
+          url="archive/read-all"
+          tclass="table table-bordered w-100 p-2" 
+          :columns="inboxColumn"
+          useNumber="1"
+          useAssigner="0"
+          useOpenButton="1"
+          @openClicked="openMail"
+          @actionUnarchive="unarchiveMail"
+          ref="inbox"
+        />
+      </div>
+      <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+        <button class="btn btn-primary mb-4"> <i class="fa fa-file"></i> New file </button>
+
+        <DataTables
+          url="archive/read-all"
+          tclass="table table-bordered w-100 p-2" 
+          :columns="inboxColumn"
+          useNumber="1"
+          useAssigner="0"
+          useOpenButton="1"
+          @openClicked="openMail"
+          @actionUnarchive="unarchiveMail"
+          ref="inbox"
+        />
+      </div>
+    </div>
   </div>  
 </template>
 
